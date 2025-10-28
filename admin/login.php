23bc89f927e36e4dc6fefd639720e47b49b47f59
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-10-28 13:01:14 +0200

    add chnges to css

--- admin/login.php
+++ admin/login.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login - LT Software</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 <body class="admin-body">
     <div class="admin-login-container">
