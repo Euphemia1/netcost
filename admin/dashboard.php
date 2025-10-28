9b678ac4389be633a13d94466746d446db101812
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-10-28 13:05:33 +0200

    add chnges to style.css

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -41,7 +41,7 @@ $contacts_result = mysqli_query($conn, "SELECT * FROM contacts ORDER BY created_
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard - LT Software</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 <body class="admin-body">
     <div class="admin-container">
