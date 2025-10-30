70aceeb3b0e3bfa5b66d25858719c019c51a95be
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-10-30 18:25:58 +0200

    delete 2nd hardcoded article

--- sql/init.sql
+++ sql/init.sql
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS news (
 
 INSERT INTO news (title, content) VALUES 
 ('New Feature Release', 'We are excited to announce the release of our new AI-powered cost estimation feature that reduces estimation time by 50%.'),
-('Partnership Announcement', 'LT Software partners with leading construction firms to bring innovative solutions to the industry.'),
+(''),
 ('');
 
 -- Contacts table
